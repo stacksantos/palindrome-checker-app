25fdd8cb90e170b2b2a6d7fe74f9ac019d18e5ee
stacksantos <stacksantos@proton.me>
2022-06-09 19:56:16 -0500

    update js

--- js/app.js
+++ js/app.js
@@ -4,28 +4,55 @@ const reset = document.querySelector("#reset");
 const input = document.querySelector("#input").value;
 const display = document.querySelector(".screenText");
 
+function checkButton() {
+  check.addEventListener("mouseup", () => {
+    const reg = /[^A-Za-z0-9_]/g;
+    const lowerCase = input.toString().toLowerCase().replace(reg, "");
+    const reverse = lowerCase.split("").reverse().join("");
+
+    function checkResults() {
+      return (lowerCase === reverse)
+      }
+    
+      if (!input) {
+        display.innerHTML = "uhh...no?"
+
+      } else {
+        display.innerHTML = checkResults().toString().toUpperCase();
+      }
+      
+    localStorage.clear();
+  });
+}
+
+checkButton();
+
+
 function buttonPress() {
   buttons.forEach((button) => {
     button.addEventListener("mousedown", () => {
       button.style.backgroundColor = "rgb(49, 0, 14)";
     });
     button.addEventListener("mouseup", () => {
-      button.style.backgroundColor = "rgb(101, 2, 29)";
+      button.style.backgroundColor = "";
     });
   });
 }
 
 buttonPress();
 
-function checkButton() {
-  check.addEventListener("mouseup", () => {
-    const reg = /[^A-Za-z0-9_]/g;
-    const lowerCase = input.toString().toLowerCase().replace(reg, "");
-    const reverse = lowerCase.split("").reverse().join("");
-    const results = lowerCase === reverse;
-
-    display.innerHTML = results.toString().toUpperCase();
-  });
+function resetApp() {
+  reset.addEventListener("mouseup", () => {
+    display.innerHTML = "TRY<br>ANOTHER";
+    setTimeout(() => {
+      location.reload();
+      document.querySelector("form").reset();
+      location.reload();
+      localStorage.clear();
+    sessionStorage.clear()
+    ;
+    }, 1300);  
+  })
 }
 
-checkButton();
+resetApp()
