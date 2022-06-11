2f3f6ccacda31f97926411d282358a812f3f747d
stacksantos <stacksantos@proton.me>
2022-06-10 21:29:20 -0500

    update app

--- js/app.js
+++ js/app.js
@@ -1,58 +1,85 @@
 const buttons = document.querySelectorAll(".button");
-const check = document.querySelector("#check");
+const submit = document.querySelector("#submit");
 const reset = document.querySelector("#reset");
-const input = document.querySelector("#input").value;
+const palindrome = document.querySelector("#palindrome").value;
 const display = document.querySelector(".screenText");
 
-function checkButton() {
-  check.addEventListener("mouseup", () => {
-    const reg = /[^A-Za-z0-9_]/g;
-    const lowerCase = input.toString().toLowerCase().replace(reg, "");
-    const reverse = lowerCase.split("").reverse().join("");
+function checkPalindrome() {
+  const reg = /[^A-Za-z0-9_]/g;
+  const lowerCase = palindrome.toString().toLowerCase().replace(reg, "");
+  const reverse = lowerCase.split("").reverse().join("");
+  let result = lowerCase === reverse;
 
-    function checkResults() {
-      return (lowerCase === reverse)
-      }
-    
-      if (!input) {
-        display.innerHTML = "uhh...no?"
+  return result.toString().toUpperCase();
+}
 
-      } else {
-        display.innerHTML = checkResults().toString().toUpperCase();
-      }
-      
-    localStorage.clear();
+if (!palindrome) {
+} else {
+
+  display.innerHTML = `${palindrome} is`;
+
+  setTimeout(() => {
+    display.innerHTML = "."
+  }, 1000);
+  setTimeout(() => {
+    display.innerHTML = ".."
+  }, 1700);
+  setTimeout(() => {
+    display.innerHTML = "..."
+  }, 2400);
+  
+  setTimeout(() => {
+  display.innerHTML = checkPalindrome() + "!"
+  }, 3500);
+
+
+
+
+  setTimeout(() => {
+    display.innerHTML = "...another?";
+    document.querySelector("form").reset();
+  }, 5200);
+  setTimeout(() => {
+    display.innerHTML = "0_0";
+  }, 6700);
+  setTimeout(() => {
+    display.innerHTML = "-_-";
+  }, 6800);
+  setTimeout(() => {
+    display.innerHTML = "0_0";
+  }, 6900);
+  setTimeout(() => {
+    display.innerHTML = "-_-";
+  }, 9600);
+  setTimeout(() => {
+    display.innerHTML = "0_0";
+  }, 9700);
+  setTimeout(() => {
+    display.innerHTML = "-_-";
+  }, 9800);
+  setTimeout(() => {
+    display.innerHTML = "0_0";
+  }, 9900);
+  setTimeout(() => {
+    display.innerHTML = "Until next time then!";
+  }, 13000);
+  setTimeout(() => {
+    display.innerHTML = "^-^";
+  }, 15300);
+  setTimeout(() => {
+    window.location.reload();
+  }, 16400);
+}
+
+submit.addEventListener("mouseup", () => {
+  window.location.reload();
+});
+
+buttons.forEach(button => {
+  button.addEventListener("mousedown", () => {
+    button.style.backgroundColor = "rgb(59, 0, 10)";
   });
-}
-
-checkButton();
-
-
-function buttonPress() {
-  buttons.forEach((button) => {
-    button.addEventListener("mousedown", () => {
-      button.style.backgroundColor = "rgb(49, 0, 14)";
-    });
-    button.addEventListener("mouseup", () => {
-      button.style.backgroundColor = "";
-    });
+  button.addEventListener("mouseup", () => {
+    button.style.backgroundColor = "";
   });
-}
-
-buttonPress();
-
-function resetApp() {
-  reset.addEventListener("mouseup", () => {
-    display.innerHTML = "TRY<br>ANOTHER";
-    setTimeout(() => {
-      location.reload();
-      document.querySelector("form").reset();
-      location.reload();
-      localStorage.clear();
-    sessionStorage.clear()
-    ;
-    }, 1300);  
-  })
-}
-
-resetApp()
+});
