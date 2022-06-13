135b738d4f7ef25d91e63bffc717561557f7d8e7
stacksantos <stacksantos@proton.me>
2022-06-13 16:52:33 -0500

    finish js logic

--- js/app.js
+++ js/app.js
@@ -3,9 +3,20 @@ const submit = document.querySelector("#submit");
 // const reset = document.querySelector("#reset");
 const display = document.querySelector("output");
 let palindrome = document.querySelector("input").value;
+const input = document.querySelector("input");
 
-display.innerHTML = "PALINDROME<br>CHECKER";
+// Start screen
+display.innerHTML = "POCKET<br>PALINDROME";
 
+// first input listener
+input.addEventListener("focusin", () => {
+  input.placeholder = "";
+});
+input.addEventListener("focusout", () => {
+  input.placeholder = "Insert Palindrome";
+});
+
+// palindrome algorithm
 function checkPalindrome() {
   const reg = /[^A-Za-z0-9_]/g;
   let palindrome = document.querySelector("input").value;
@@ -16,6 +27,139 @@ function checkPalindrome() {
   return result;
 }
 
+// first text to display
+function initialDisplay() {
+  let palindrome = document.querySelector("input").value;
+  let cancel = false;
+  let done = true;
+
+  display.innerHTML = `${palindrome} is`;
+
+  setTimeout(() => {
+    display.innerHTML = ".";
+  }, 1000);
+  setTimeout(() => {
+    display.innerHTML = "..";
+  }, 1700);
+  setTimeout(() => {
+    display.innerHTML = "...";
+  }, 2400);
+  setTimeout(() => {
+    display.innerHTML = `${checkPalindrome().toString().toUpperCase()}!`;
+  }, 3500);
+  setTimeout(() => {
+    display.innerHTML = "...another?";
+    document.querySelector("input").value = "";
+  }, 5200);
+
+  function endDisplay() {
+    setTimeout(() => {
+      input.addEventListener("focusin", () => {
+        cancel = true;
+      });
+      if ((cancel === true)) {
+        display.innerHTML = "What's next?";
+      } else {
+        display.innerHTML = "0_0";
+      }
+    }, 6700);
+    setTimeout(() => {
+      input.addEventListener("focusin", () => {
+        cancel = true;
+      });
+      if ((cancel === true)) {
+        display.innerHTML = "What's next?";
+      } else {
+        display.innerHTML = "-_-";
+      }
+    }, 6800);
+    setTimeout(() => {
+      input.addEventListener("focusin", () => {
+        cancel = true;
+      });
+
+      if ((cancel === true)) {
+        display.innerHTML = "What's next?";
+      } else {
+        display.innerHTML = "0_0";
+      }
+    }, 6900);
+    setTimeout(() => {
+      input.addEventListener("focusin", () => {
+        cancel = true;
+      });
+
+      if ((cancel === true)) {
+        display.innerHTML = "What's next?";
+      } else {
+        display.innerHTML = "-_-";
+      }
+    }, 9600);
+    setTimeout(() => {
+      input.addEventListener("focusin", () => {
+        cancel = true;
+      });
+
+      if ((cancel === true)) {
+        display.innerHTML = "What's next?";
+      } else {
+        display.innerHTML = "0_0";
+      }
+    }, 9700);
+    setTimeout(() => {
+      input.addEventListener("focusin", () => {
+        cancel = true;
+      });
+
+      if ((cancel === true)) {
+        display.innerHTML = "What's next?";
+      } else {
+        display.innerHTML = "-_-";
+      }
+    }, 9800);
+    setTimeout(() => {
+      input.addEventListener("focusin", () => {
+        cancel = true;
+      });
+
+      if ((cancel === true)) {
+        display.innerHTML = "What's next?";
+      } else {
+        display.innerHTML = "0_0";
+      }
+    }, 9900);
+    setTimeout(() => {
+      input.addEventListener("focusin", () => {
+        cancel = true;
+      });
+
+      if ((cancel === true)) {
+        display.innerHTML = "What's next?";
+      } else {
+        display.innerHTML = "Until next time then!";
+      }
+    }, 11000);
+    setTimeout(() => {
+      input.addEventListener("focusin", () => {
+        cancel = true;
+      });
+
+      if ((cancel === true)) {
+        display.innerHTML = "What's next?";
+      } else {
+        display.innerHTML = "^-^";
+      }
+    }, 13300);
+  }
+
+  if (cancel === false) {
+    endDisplay();
+  }
+}
+
+// second text after continue
+
+// display results function
 function displayResult() {
   submit.addEventListener("mouseup", () => {
     let palindrome = document.querySelector("input").value;
@@ -23,67 +167,13 @@ function displayResult() {
     if (!palindrome) {
       display.innerHTML = "You forgot<br> something...";
     } else {
-      display.innerHTML = `${palindrome} is`;
-
-      setTimeout(() => {
-        display.innerHTML = ".";
-      }, 1000);
-      setTimeout(() => {
-        display.innerHTML = "..";
-      }, 1700);
-      setTimeout(() => {
-        display.innerHTML = "...";
-      }, 2400);
-
-      setTimeout(() => {
-        display.innerHTML = `${checkPalindrome().toString().toUpperCase()}!`;
-      }, 3500);
-
-      setTimeout(() => {
-        display.innerHTML = "...another?";
-        document.querySelector("input").value = "";
-      }, 5200);
-
-      setTimeout(() => {
-        display.innerHTML = "0_0";
-      }, 6700);
-
-      setTimeout(() => {
-        display.innerHTML = "-_-";
-      }, 6800);
-
-      setTimeout(() => {
-        display.innerHTML = "0_0";
-      }, 6900);
-
-      setTimeout(() => {
-        display.innerHTML = "-_-";
-      }, 9600);
-
-      setTimeout(() => {
-        display.innerHTML = "0_0";
-      }, 9700);
-
-      setTimeout(() => {
-        display.innerHTML = "-_-";
-      }, 9800);
-
-      setTimeout(() => {
-        display.innerHTML = "0_0";
-      }, 9900);
-
-      setTimeout(() => {
-        display.innerHTML = "Until next time then!";
-      }, 13000);
-
-      setTimeout(() => {
-        display.innerHTML = "^-^";
-      }, 15300);
+      initialDisplay();
     }
   });
 }
 displayResult();
 
+// button color changer
 function buttonColor() {
   submit.addEventListener("mousedown", () => {
     submit.style.backgroundColor = "rgb(59, 0, 10)";
@@ -92,4 +182,5 @@ function buttonColor() {
     submit.style.backgroundColor = "";
   });
 }
+
 buttonColor();
